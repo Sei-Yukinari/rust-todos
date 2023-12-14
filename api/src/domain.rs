cb556336edf75a06dfeeb03edc35cc7d0fefa422
Sei Yukinari <sei.yukinari@nextremer.com>
2023-12-14 15:27:24 +0900

    refactor mod file

--- api/src/domain.rs
+++
@@ -1,4 +0,0 @@
-pub mod entity;
-pub mod error;
-
-pub mod repository;
--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,12 +1,9 @@
 extern crate core;
 
-pub mod presentation;
-pub mod use_case;
-pub mod create_app;
 pub mod domain;
-
-pub mod infrastructure;
-
 pub mod gateway;
-
+pub mod infrastructure;
+pub mod use_case;
+pub mod presentation;
+pub mod create_app;
 pub mod dependency_injection;
