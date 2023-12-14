d23eeaf3d19dbe88af0afa79fb337b4e237564e6
Sei Yukinari <sei.yukinari@nextremer.com>
2023-12-14 16:29:20 +0900

    add domain test

--- api/src/domain/entity/user.rs
+++ api/src/domain/entity/user.rs
@@ -22,3 +22,34 @@ impl User {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn user_new_creates_user_from_row() {
+        let row = UserRow {
+            id: 1,
+            name: "Test User".to_string(),
+        };
+
+        let user = User::new(row.clone());
+
+        assert_eq!(user.id, row.id);
+        assert_eq!(user.name, row.name);
+    }
+
+    #[tokio::test]
+    async fn user_new_handles_empty_name() {
+        let row = UserRow {
+            id: 1,
+            name: "".to_string(),
+        };
+
+        let user = User::new(row.clone());
+
+        assert_eq!(user.id, row.id);
+        assert_eq!(user.name, row.name);
+    }
+}
