beefc427ca9d430e717a48e065b55a6f8bfd8bec
Sei Yukinari <sei.yukinari@nextremer.com>
2023-12-20 15:43:54 +0900

    fix type

--- api/src/bin/generate-schema.rs
+++ api/src/bin/generate-schema.rs
@@ -1,15 +1,14 @@
 use std::io::Write;
+use std::sync::Arc;
+
 use api::context::Context;
-// use api::{
-//     context::Context,
-// };
 use api::infrastructure::db::postgres::db_pool;
 use api::schema::create_schema;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let pool = db_pool().await;
-    let ctx = Context::new(pool);
+    let ctx = Arc::new(Context::new(pool));
     let schema = create_schema(ctx);
     let mut sdl_file = std::fs::File::create("./schema/schema.graphql")?;
     write!(&mut sdl_file, "{}", &schema.sdl()).expect("error");
