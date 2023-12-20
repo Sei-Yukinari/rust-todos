d37687d61ecd285fa78d8dcfafe59702d7fea413
Sei Yukinari <sei.yukinari@nextremer.com>
2023-12-20 15:46:57 +0900

    add variable

--- api/src/create_app.rs
+++ api/src/create_app.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+
 use actix_web::{App, guard, HttpResponse, Result, web};
 use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceFactory, ServiceRequest, ServiceResponse};
@@ -20,7 +21,7 @@ pub fn create_app(pool: Pool<Postgres>) -> App<
         Error=Error,
     >,
 > {
-    let ctx =  Arc::new(Context::new(pool));
+    let ctx = Arc::new(Context::new(pool));
     let schema = create_schema(ctx);
 
     App::new()
--- api/src/graphql/resolver/user_resolver.rs
+++ api/src/graphql/resolver/user_resolver.rs
@@ -11,12 +11,12 @@ pub struct UsersQuery {}
 
 #[Object]
 impl UsersQuery {
-    async fn logged_in_user(&self, ctx: &Context<'_>) -> Result<Option<User>> {
+    async fn logged_in_user(&self, ctx: &Context<'_>, id: i64) -> Result<Option<User>> {
         let usecase = &ctx
             .data_unchecked::<Arc<AppContext>>()
             .user_use_case;
         let user = usecase
-            .find_user_by_id(1)
+            .find_user_by_id(id)
             .await?
             .map(User::new);
         Ok(user)
